99c6442dd7e2a01ac36c170c6d7d0ec9bc373f5e
MHDOA <mohammadali4a@gmail.com>
2025-11-25 20:22:19 +0330

    Add LCD Library & Proteus
    Add simple lcd library. can printing string, initialize lcd and sending commands. add proteus to show test project write Hello on LCD. can change Pins in the defines.

---
+++ Smart-Air-Condition-System/Smart-Air-Condition-System/LCD/lcd.h
@@ -0,0 +1,67 @@
+﻿#ifndef LCD_H_
+#define LCD_H_
+
+#define F_CPU 8000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define LCD_PORT_DATA   PORTD
+#define LCD_DDR_DATA    DDRD
+#define LCD_PORT_CTRL   PORTC
+#define LCD_DDR_CTRL    DDRC
+
+#define RS PC0
+#define EN PC1
+
+void lcd_cmd(unsigned char cmd);
+void lcd_data(unsigned char data);
+void lcd_init();
+void lcd_print(char *str);
+
+static void lcd_send_halfbyte(unsigned char halfbit)
+{
+	LCD_PORT_DATA = (LCD_PORT_DATA & 0x0F) | (halfbit & 0xF0);
+	LCD_PORT_CTRL |= (1 << EN);
+	_delay_us(1);
+	LCD_PORT_CTRL &= ~(1 << EN);
+	_delay_us(100);
+}
+
+void lcd_cmd(unsigned char cmd)
+{
+	LCD_PORT_CTRL &= ~(1 << RS);
+	lcd_send_halfbyte(cmd);
+	lcd_send_halfbyte(cmd << 4);
+}
+
+void lcd_data(unsigned char data)
+{
+	LCD_PORT_CTRL |= (1 << RS);
+	lcd_send_halfbyte(data);
+	lcd_send_halfbyte(data << 4);
+}
+
+void lcd_init()
+{
+	LCD_DDR_DATA |= 0xF0;    
+	LCD_DDR_CTRL |= (1 << RS) | (1 << EN);
+
+	_delay_ms(20);
+
+	lcd_cmd(0x02);   // 4bit
+	lcd_cmd(0x28);   // 2 lines
+	lcd_cmd(0x0C);   // Display ON, cursor off
+	lcd_cmd(0x06);   // Auto-increment
+	lcd_cmd(0x01);   // Clear
+	_delay_ms(2);
+}
+
+void lcd_print(char *str)
+{
+	while(*str)
+	{
+		lcd_data(*str++);
+	}
+}
+
+#endif
--- Smart-Air-Condition-System/Smart-Air-Condition-System/main.c
+++ Smart-Air-Condition-System/Smart-Air-Condition-System/main.c
@@ -1,13 +1,16 @@
 #define F_CPU 8000000UL
 #include <avr/io.h>
 #include <util/delay.h>
-
+#include "LCD/lcd.h"
 
 
 int main(void)
 {
+	lcd_init();
+	unsigned char message[] = "Hello";
 	while (1)
 	{
-		// loop
+		lcd_print(message);
+		_delay_ms(500);
 	}
 }
