2065dc3ce0628621e499c5b01fceaa9b538d36c0
MHDOA <mohammadali4a@gmail.com>
2026-02-07 13:59:21 +0330

    Add UART Lib
    Add uart serial library to can make relation to sim800l madule

---
+++ Smart-Air-Condition-System/Smart-Air-Condition-System/UART/uart.c
@@ -0,0 +1,20 @@
+﻿#include <avr/io.h>
+#include "uart.h"
+
+#define UBRR_VAL ((F_CPU/16/BAUD)-1)
+
+void uart_init(void) {
+	UBRRH = (unsigned char)(UBRR_VAL>>8);
+	UBRRL = (unsigned char)UBRR_VAL;
+	UCSRB = (1<<TXEN);                     // only TX
+	UCSRC = (1<<URSEL)|(1<<UCSZ1)|(1<<UCSZ0); // 8bit
+}
+
+void uart_send_char(char c) {
+	while (!(UCSRA & (1<<UDRE)));
+	UDR = c;
+}
+
+void uart_send_str(const char *s) {
+	while (*s) uart_send_char(*s++);
+}
---
+++ Smart-Air-Condition-System/Smart-Air-Condition-System/UART/uart.h
@@ -0,0 +1,13 @@
+﻿#ifndef UART_H_
+#define UART_H_
+
+#include <avr/io.h>
+
+#define F_CPU 4000000UL
+#define BAUD 9600
+
+void uart_init(void);
+void uart_send_char(char c);
+void uart_send_str(const char *s);
+
+#endif /* UART_H_ */
