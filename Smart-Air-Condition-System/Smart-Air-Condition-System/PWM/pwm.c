a5dcafde93201f2e53f08d42dae406d96b1b58a4
MHDOA <mohammadali4a@gmail.com>
2026-02-07 23:04:51 +0330

    Add PWM Lib
    Add pwm lib to use that in lm35 code and pwm on cooler and hiter pin

---
+++ Smart-Air-Condition-System/Smart-Air-Condition-System/PWM/pwm.c
@@ -0,0 +1,18 @@
+﻿#include "pwm.h"
+#include <avr/io.h>
+
+void PWM_Init(void) {
+    DDRB |= (1 << PB1) | (1 << PB2); // OC1A, OC1B
+
+    TCCR1A = (1 << COM1A1) | (1 << COM1B1) | (1 << WGM10);
+    TCCR1B = (1 << WGM12) | (1 << CS11);
+}
+
+void Analog_Write(uint8_t pin_pwm, uint8_t duty) {
+    if(duty > 255) duty = 255;
+
+    if(pin_pwm == PWM_PIN_PB1)
+        OCR1A = duty;
+    else if(pin_pwm == PWM_PIN_PB2)
+        OCR1B = duty;
+}
---
+++ Smart-Air-Condition-System/Smart-Air-Condition-System/PWM/pwm.h
@@ -0,0 +1,13 @@
+﻿#ifndef PWM_H_
+#define PWM_H_
+
+#include <avr/io.h>
+
+#define PWM_PIN_PB1 1
+#define PWM_PIN_PB2 2
+
+void PWM_Init(void);
+
+void Analog_Write(uint8_t pin_pwm, uint8_t duty);
+
+#endif /* PWM_H_ */
